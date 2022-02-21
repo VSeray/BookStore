e023f125aeab432731cdc896718038a178daa752
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-02-21 10:12:39 +0200

    Create components in files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Router, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Navbar />
+      <Router>
+        <Route path="/books">
+          <Books />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="/">
+          <Navigate to="/books" />
+        </Route>
+      </Router>
+    </main>
   );
 }
 
